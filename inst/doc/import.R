f0496526c6f09dfe7ce0b077a6097e5af6cc2371
H. Sherry Zhang <huize.zhang@monash.edu>
2022-06-02 11:30:06 +0000

    version 0.1.1

--- inst/doc/import.R
+++ inst/doc/import.R
@@ -10,47 +10,30 @@ library(cubble)
 library(dplyr)
 
 ## ----echo = FALSE-------------------------------------------------------------
-latlon_ref <- tibble::tibble(id = c("sydney", "melbourne"),
-                         lat = c(-33.8675, -37.8136),
-                         long = c(151.2070, 144.9631))
-
-master_list <- climate_aus %>%  
-   as_tibble() %>%  
-   select(id, lat, long, elev, name, wmo_id)
-
-calc_dist <- function(df_ref, df, n = 5){
-  long_ref <- df_ref$long
-  lat_ref <- df_ref$lat
-  
-  df %>%  
-    mutate(long_ref = long_ref,
-           lat_ref=  lat_ref,
-           dist = rnoaa::meteo_spherical_distance(lat, long,lat_ref, long_ref)) %>%  
-    slice_min(dist, n = n) %>%  
-    mutate(city = df_ref$id) %>%  
-    select(-long_ref, -lat_ref)
-}
-
-stations <- do.call("rbind", purrr::map(1:nrow(latlon_ref), 
-                             ~calc_dist(latlon_ref[.x, ], master_list)))
+syd_all <- climate_aus %>% slice_nearby(coord = c(151.207, -33.867), n = 5)
+syd <- syd_all %>% select(id:wmo_id)
 
 ## -----------------------------------------------------------------------------
-stations
+syd
 
-## -----------------------------------------------------------------------------
-raw <- stations %>%  
-  rowwise() %>%  
-  mutate(ts = list(rnoaa::meteo_pull_monitors(id, 
-                                       date_min = "2020-01-01", 
-                                       date_max = "2020-12-31",
-                                       var = c("PRCP", "TMAX", "TMIN")) %>%  select(-id))) 
+## ----eval = FALSE-------------------------------------------------------------
+#  raw <- syd %>%
+#    rowwise() %>%
+#    mutate(ts = list(rnoaa::meteo_pull_monitors(id,
+#                                         date_min = "2020-01-01",
+#                                         date_max = "2020-12-31",
+#                                         var = c("PRCP", "TMAX", "TMIN")) %>%  select(-id)))
+#  raw
+
+## ----echo = FALSE-------------------------------------------------------------
+raw <- syd_all %>% select(-c(long_ref, lat_ref, dist)) %>% rowwise()
 raw
 
 ## -----------------------------------------------------------------------------
-sydmel_climate <- raw %>%  
+syd_climate <- raw %>%  
   as_cubble(key = id, index = date, coords = c(long, lat))
 
-sydmel_climate
+syd_climate
 
 ## ----echo = FALSE-------------------------------------------------------------
 dt <- climate_flat %>%  
