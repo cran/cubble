1125a2774054e335ef6be7c538f59a7a63ec12b4
H. Sherry Zhang <huizezhangsh@gmail.com>
2024-07-18 11:10:02 +0000

    version 0.3.2

--- R/as-cubble.R
+++ R/as-cubble.R
@@ -15,6 +15,7 @@
 #' to downsample
 #' @param crs used in `as_cubble.tbl_df()` to set the crs.
 #' the data to read in `as_cubble.netcdf()`.
+#' @param dimensions used when creating a cubble from a stars object
 #' @importFrom tidyr unchop
 #' @importFrom tsibble index
 #' @export
@@ -63,7 +64,7 @@ as_cubble.data.frame <- function(data, key, index, coords, ...){
 
 #' @rdname as_cubble
 #' @export
-as_cubble.tbl_df <- function(data, key, index, coords, crs, ...) {
+as_cubble.tbl_df <- function(data, key, index, coords, crs, dimensions, ...) {
   if (is_tsibble(data)){
     key <- sym(tsibble::key_vars(data))
     index <- sym(tsibble::index(data))
@@ -99,7 +100,9 @@ as_cubble.tbl_df <- function(data, key, index, coords, crs, ...) {
     data, key = as_name(key), index = as_name(index), coords = coords
   )
 
-  if (!missing(crs)) res <- res |> make_spatial_sf(crs = crs)
+  #if (!missing(crs)) res <- res |> make_spatial_sf(crs = crs)
+  if (!missing(crs)) attr(res, "crs") <- crs
+  if (!missing(dimensions)) attr(res, "dimensions") <- dimensions
   return(res)
 
 
@@ -199,8 +202,7 @@ as_cubble.stars <- function(data, key, index, coords, ...){
       group_by(!!!map(longlat, sym)) |>
       mutate(id = dplyr::cur_group_id()) |>
       ungroup() |>
-      as_cubble(key = id, index = !!index, coords = longlat) |>
-      make_spatial_sf(crs = sf::st_crs(data))
+      as_cubble(key = id, index = !!index, coords = longlat, dimension = stars::st_dimensions(data))
   }
 }
 
